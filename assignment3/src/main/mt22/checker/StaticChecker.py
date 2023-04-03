07e35c09a069821272aa805d8ae6e971c962727f
SonTheeTheson2002SonThee <theson2604@gmail.com>
2023-04-03 15:34:25 +0700

    commit

--- assignment3/src/main/mt22/checker/StaticChecker.py
+++ assignment3/src/main/mt22/checker/StaticChecker.py
@@ -1,5 +1,24 @@
-from Visitor import Visitor
+from StaticError import *
+from Utils import Utils
+from Visitor import *
+from AST import *
 
 
 class StaticChecker(Visitor):
     pass
+
+    
+
+    # Visit literal => return corresponding type
+    def visitIntLit(self, ctx, o):
+        return IntegerType()
+    
+    def visitFloatLit(self, ctx, o):
+        return FloatType()
+    
+    def visitBoolLit(self, ctx, o):
+        return BooleanType()
+    
+    def visitStringLit(self, ctx, o):
+        return StringType()
+        
